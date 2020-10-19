de9bd59866972badaeb142270be2cbbca93b4a7c
excursive <excursiveart@gmail.com>
2020-10-19 16:10:04 -0700

    edit

--- manage_software.sh
+++ manage_software.sh
@@ -20,6 +20,38 @@ sha256r() {
   fi
 }
 
+# arguments:
+# 1 = sha1 hash of git repo to checkout
+# 2 = if 2 = 'fetch_updates' then fetch remote changes, otherwise ignored
+clean_and_update_repo() {
+  printf '\n======== Cleaning git repo and checking out specified commit\n'
+  if ! is_valid_sha1 "$1"; then
+    printf '\n==== Error: commit is not a valid sha1 hash\n'
+    exit 1
+  fi
+  git clean -d -f -f -x
+  if [ "$2" = 'fetch_updates' ]; then
+    git fetch --recurse-submodules=on-demand
+    if [ "$?" != 0 ]; then
+      printf '\n==== Warning: Could not fetch updates\n'
+    fi
+  fi
+  git checkout --recurse-submodules "$1"
+  if [ "$?" != 0 ]; then
+    printf '\n==== Error: Could not checkout specified commit\n'
+    exit 1
+  fi
+  git submodule deinit --force --all
+  git reset --hard --recurse-submodules "$1"
+  git clean -d -f -f -x
+  git checkout --recurse-submodules "$1"
+  git submodule update --init --force --checkout --recursive --
+  if [ "$?" != 0 ]; then
+    printf '\n==== Error: Could not checkout submodules\n'
+    exit 1
+  fi
+}
+
 manage_aseprite() {
   printf '\n======== Creating directories\n'
   asepritedir="${2}/aseprite"
@@ -48,19 +80,18 @@ manage_aseprite() {
       printf '\nError: Missing files from original installation\n'
       exit 1
     fi
-    # need a better way to check if skia installation matches checksums
+    # TODO: need a better way to check if skia installation matches checksums
     if ! cd "${skiainstalldir}" \
-       || ! diff "${asepritedir}/skia-aseprite-m81-sha256sums.txt" <(sha256r); then
+       || ! sha256sum -c --quiet "${asepritedir}/skia-aseprite-m81-sha256sums.txt"; then
       printf '\nError: Skia installation does not match checksums\n'
       exit 1
     fi
-    
     if ! mkdir "${aseinstalldir}" \
                "${builddir}" "${asebuilddir}" "${skiabuilddir}" \
                "${tempbindir}"; then
       printf '\nError: Could not create build directories\n'
+      exit 1
     fi
-    
   else
     if [ -e "${asepritedir}" ] \
        || ! mkdir "${asepritedir}" "${aseinstalldir}" "${skiainstalldir}" \
@@ -76,44 +107,43 @@ manage_aseprite() {
   # set up a temp folder with a link to temporarily add to path
   ln -s '/usr/bin/python2' "${tempbindir}/python"
   
-  
-  printf '\n======== Checking out git repositories\n'
-  cd "${srcdir}"
-  #git clone --no-checkout 'https://chromium.googlesource.com/chromium/tools/depot_tools.git'
-  git clone --no-checkout 'https://skia.googlesource.com/skia.git'
-  git clone --no-checkout 'https://github.com/aseprite/aseprite.git'
+  if [ "$1" = 'install' ]; then
+    printf '\n======== Checking out git repositories\n'
+    cd "${srcdir}"
+    #git clone --no-checkout 'https://chromium.googlesource.com/chromium/tools/depot_tools.git'
+    git clone --no-checkout 'https://skia.googlesource.com/skia.git'
+    git clone --no-checkout 'https://github.com/aseprite/aseprite.git'
+  fi
   
   printf '\n======== Checking out aseprite version 1.2.25\n'
   cd "${asesrcdir}"
   if [ "$(git config --get remote.origin.url)" != 'https://github.com/aseprite/aseprite.git' ] \
-      || [ "$(git ls-remote --get-url origin)" != 'https://github.com/aseprite/aseprite.git' ] \
-           || [ "$(git remote get-url origin)" != 'https://github.com/aseprite/aseprite.git' ]; then
+     || [ "$(git remote get-url --all origin)" != 'https://github.com/aseprite/aseprite.git' ] \
+      || [ "$(git ls-remote --get-url origin)" != 'https://github.com/aseprite/aseprite.git' ]; then
     printf '\nError: aseprite git repository url does not match\n'
     exit 1
   fi
-  git checkout f44aad06db9d7a7efe9beb0038df37140ac9c2ba
-  git submodule update --init --recursive
+  clean_and_update_repo 'f44aad06db9d7a7efe9beb0038df37140ac9c2ba'
   
   # just check out a random depot_tools commit that is known to work
   #cd "${depottoolsdir}"
   #if [ "$(git config --get remote.origin.url)" != 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' ] \
-  #    || [ "$(git ls-remote --get-url origin)" != 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' ] \
-  #         || [ "$(git remote get-url origin)" != 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' ]; then
+  #   || [ "$(git remote get-url --all origin)" != 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' ] \
+  #    || [ "$(git ls-remote --get-url origin)" != 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' ]; then
   #  printf '\nError: depot_tools git repository url does not match\n'
   #  exit 1
   #fi
-  #git checkout b073999c6f90103a36a923e63ae8cf7a5c9c6c8c
-  
+  #clean_and_update_repo 'b073999c6f90103a36a923e63ae8cf7a5c9c6c8c'
   
   printf '\n======== Checking out commit aseprite-m81 skia was forked from\n'
   cd "${skiasrcdir}"
   if [ "$(git config --get remote.origin.url)" != 'https://skia.googlesource.com/skia.git' ] \
-      || [ "$(git ls-remote --get-url origin)" != 'https://skia.googlesource.com/skia.git' ] \
-         || [ "$(git remote --get-url origin)" != 'https://skia.googlesource.com/skia.git' ]; then
+     || [ "$(git remote get-url --all origin)" != 'https://skia.googlesource.com/skia.git' ] \
+      || [ "$(git ls-remote --get-url origin)" != 'https://skia.googlesource.com/skia.git' ]; then
     printf '\nError: skia git repository url does not match\n'
     exit 1
   fi
-  git checkout 3e98c0e1d11516347ecc594959af2c1da4d04fc9
+  clean_and_update_repo '3e98c0e1d11516347ecc594959af2c1da4d04fc9'
   PATH="${PATH}:${tempbindir}" python tools/git-sync-deps
   
   printf '\n==== Modifying files to match aseprite-m81 skia\n'
@@ -124,7 +154,8 @@ manage_aseprite() {
 static inline double sk_ieee_double_divide_TODO_IS_DIVIDE_BY_ZERO_SAFE_HERE(double n, double d) {' \
                'include/private/SkFloatingPoint.h'
   sed -i -e '66c\
-    # Setup the env before\n    #env_setup = "cmd /c $win_sdk\\\\bin\\\\SetEnv.cmd /x86 \&\& "' \
+    # Setup the env before\
+    #env_setup = "cmd /c $win_sdk\\\\bin\\\\SetEnv.cmd /x86 \&\& "' \
                'gn/toolchain/BUILD.gn'
   sed -i -e '25i\
     include_dirs = [ "../externals/freetype/include" ]' \
@@ -196,9 +227,9 @@ static inline double sk_ieee_double_divide_TODO_IS_DIVIDE_BY_ZERO_SAFE_HERE(doub
   
   printf '\n======== Generating checksums\n'
   cd "${aseinstalldir}"
-  sha256r 'aseprite-1_2_25-sha256sums.txt'
+  sha256r "${asepritedir}/aseprite-1_2_25-sha256sums.txt"
   cd "${skiainstalldir}"
-  sha256r 'skia-aseprite-m81-sha256sums.txt'
+  sha256r "${asepritedir}/skia-aseprite-m81-sha256sums.txt"
   
   
   printf '\n======== Cleaning up\n'
@@ -291,7 +322,7 @@ case "$1" in
     manage_aseprite "$action" "$version" "$install_parent_directory" "$keep_sources"
   ;;
   '')
-    printf '\nError: No parameters given, see -h or --help\n'
+    printf '\nError: No arguments supplied, see -h or --help\n'
     exit 1
   ;;
   *)
