df70b3c4654ff7d84379b7b39efee8d340f28a72
excursive <excursiveart@gmail.com>
2021-12-21 15:29:21 -0800

    edit

--- linux/ubuntu_packages.sh
+++ linux/ubuntu_packages.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 printf '\n\n\n================\n'
 printf       '     general    \n'
-printf       '================\n'
+printf       '================\n\n'
 
 
 sudo apt-get purge unattended-upgrades
@@ -22,7 +22,7 @@ sudo apt-get install ubuntu-restricted-extras build-essential gcc-multilib g++-m
 
 printf '\n\n\n================\n'
 printf       '     mozjpeg    \n'
-printf       '================\n'
+printf       '================\n\n'
 
 #====main:
 sudo apt-get install libtool
@@ -34,7 +34,7 @@ sudo apt-get install libtool
 
 printf '\n\n\n================\n'
 printf       '      godot     \n'
-printf       '================\n'
+printf       '================\n\n'
 
 #====main:
 sudo apt-get install pkg-config libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libglu1-mesa-dev libasound2-dev libpulse-dev libfreetype6-dev libssl-dev libudev-dev libxi-dev libxrandr-dev
@@ -52,7 +52,7 @@ sudo apt-get install scons
 
 printf '\n\n\n================\n'
 printf       '    aesprite    \n'
-printf       '================\n'
+printf       '================\n\n'
 
 #====main:
 sudo apt-get install g++ cmake libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev curl libfreetype6-dev libgif-dev libharfbuzz-dev libjpeg-dev libpng-dev libpixman-1-dev zlib1g-dev
@@ -64,7 +64,7 @@ sudo apt-get install ninja-build
 
 printf '\n\n\n================\n'
 printf       '      skia      \n'
-printf       '================\n'
+printf       '================\n\n'
 
 #====main:
 sudo apt-get install build-essential libfontconfig1-dev libfreetype6-dev libgif-dev libgl1-mesa-dev libglu1-mesa-dev libharfbuzz-dev libicu-dev libjpeg-dev libpng-dev libwebp-dev
@@ -77,7 +77,7 @@ sudo apt-get install build-essential libfontconfig1-dev libfreetype6-dev libgif-
 
 printf '\n\n\n================\n'
 printf       '     dolphin    \n'
-printf       '================\n'
+printf       '================\n\n'
 
 #====main:
 sudo apt-get install libevdev-dev libudev-dev libxrandr-dev libxi-dev libpangocairo-1.0-0 git cmake make gcc g++
@@ -98,7 +98,7 @@ sudo apt-get install ffmpeg libavcodec-dev libavformat-dev libavutil-dev libswsc
 
 printf '\n\n\n================\n'
 printf       '      rpcs3     \n'
-printf       '================\n'
+printf       '================\n\n'
 
 #====main:
 sudo apt-get install build-essential libasound2-dev libpulse-dev zlib1g-dev libedit-dev libvulkan-dev libudev-dev libevdev-dev
@@ -113,7 +113,7 @@ sudo apt-get install libopenal-dev libglew-dev libsdl2-2.0-0 libsdl2-dev
 
 printf '\n\n\n================\n'
 printf       '      PCSX2     \n'
-printf       '================\n'
+printf       '================\n\n'
 
 #====main:
 sudo apt-get install cmake gcc-multilib g++-multilib libaio-dev:i386 libbz2-dev:i386 libegl1-mesa-dev:i386 libgles2-mesa-dev:i386 libjpeg-dev:i386 zlib1g-dev:i386 libjack-jackd2-dev:i386 liblzma-dev:i386 libxml2-dev:i386 libpcap0.8-dev:i386
@@ -121,7 +121,7 @@ sudo apt-get install cmake gcc-multilib g++-multilib libaio-dev:i386 libbz2-dev:
 #====universe:
 sudo apt-get install libgtk2.0-dev:i386 libsdl1.2-dev:i386 libwxgtk3.0-gtk3-dev:i386 libsdl2-dev:i386 libportaudiocpp0:i386 portaudio19-dev:i386 libsoundtouch-dev:i386
 
-# not in repo in 20.04+?
+# not in repos in 20.04+?
 #sudo apt-get install libglew-dev:i386
 
 #====multiverse (optional):
@@ -131,7 +131,7 @@ sudo apt-get install libgtk2.0-dev:i386 libsdl1.2-dev:i386 libwxgtk3.0-gtk3-dev:
 
 printf '\n\n\n================\n'
 printf       '   DuckStation  \n'
-printf       '================\n'
+printf       '================\n\n'
 
 #====main:
 sudo apt-get install cmake libxrandr-dev pkg-config libevdev-dev libwayland-dev libwayland-egl-backend-dev
@@ -152,5 +152,5 @@ sudo apt-get install libsdl2-dev extra-cmake-modules
 
 printf '\n\n\n================\n'
 printf       '    All done!   \n'
-printf       '================\n'
+printf       '================\n\n'
 
