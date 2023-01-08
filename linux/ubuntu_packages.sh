364de4d8a3b18741ec7f7f2d727f2c27d355ae36
excursive <excursiveart@gmail.com>
2023-01-08 15:19:32 -0800

    edit

--- linux/ubuntu_packages.sh
+++ linux/ubuntu_packages.sh
@@ -25,15 +25,15 @@ sudo apt-get install cd-paranoia libcdio-utils cdrdao mkcue libcue-dev cue2toc l
 
 
 
+
 printf '\n\n\n================\n'
 printf       '     mozjpeg    \n'
 printf       '================\n\n'
 
-#====main:
 sudo apt-get install libtool
 
-#====universe (optional):
 #====sudo apt-get install nasm
+
 
 
 
@@ -41,17 +41,15 @@ printf '\n\n\n================\n'
 printf       '      godot     \n'
 printf       '================\n\n'
 
-#====main:
 sudo apt-get install pkg-config libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libglu1-mesa-dev libasound2-dev libpulse-dev libfreetype6-dev libssl-dev libudev-dev libxi-dev libxrandr-dev
 
-#====main (also included in godot repo):
+# (also included in godot repo):
 sudo apt-get install libfreetype-dev libogg-dev libpng-dev libtheora-dev libvorbis-dev libvpx-dev libwebp-dev libminiupnpc-dev libopus-dev libpcre2-dev zlib1g-dev libzstd-dev
 
-#====universe:
 sudo apt-get install scons
 
-#====universe (optional):
 #====sudo apt-get install yasm
+
 
 
 
@@ -59,11 +57,10 @@ printf '\n\n\n================\n'
 printf       '    aesprite    \n'
 printf       '================\n\n'
 
-#====main:
 sudo apt-get install g++ cmake libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev curl libfreetype6-dev libgif-dev libharfbuzz-dev libjpeg-dev libpng-dev libpixman-1-dev zlib1g-dev
 
-#====universe:
 sudo apt-get install ninja-build
+
 
 
 
@@ -71,12 +68,10 @@ printf '\n\n\n================\n'
 printf       '      skia      \n'
 printf       '================\n\n'
 
-#====main:
 sudo apt-get install build-essential libfontconfig1-dev libfreetype6-dev libgif-dev libgl1-mesa-dev libglu1-mesa-dev libharfbuzz-dev libicu-dev libjpeg-dev libpng-dev libwebp-dev
 
-
-#====universe (optional):
 #====sudo apt-get install freeglut3-dev
+
 
 
 
@@ -84,20 +79,17 @@ printf '\n\n\n================\n'
 printf       '     dolphin    \n'
 printf       '================\n\n'
 
-#====main:
-sudo apt-get install libevdev-dev libudev-dev libxrandr-dev libxi-dev libpangocairo-1.0-0 git cmake make gcc g++
+sudo apt-get install libevdev-dev libudev-dev libxrandr-dev libxi-dev libpangocairo-1.0-0 git cmake make gcc g++ ffmpeg libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libmbedtls-dev
 
-#====main (also included in dolphin repo):
+# (also included in dolphin repo):
 sudo apt-get install ca-certificates pkg-config libminiupnpc-dev libcurl4-openssl-dev libsystemd-dev libbluetooth-dev libasound2-dev libpulse-dev libbz2-dev libzstd-dev liblzo2-dev libpng-dev libusb-1.0-0-dev gettext
 
-#====universe:
-sudo apt-get install ffmpeg libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
-
-#====universe (qt5 packages):
+# (qt5 packages):
 #====sudo apt-get install qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qtbase5-private-dev
 
-#====universe (also included in dolphin repo):
-#====sudo apt-get install libsfml-dev libmbedtls-dev libhidapi-dev libpugixml-dev
+# (also included in dolphin repo):
+#====sudo apt-get install libsfml-dev libhidapi-dev libpugixml-dev
+
 
 
 
@@ -105,14 +97,13 @@ printf '\n\n\n================\n'
 printf       '      rpcs3     \n'
 printf       '================\n\n'
 
-#====main:
 sudo apt-get install build-essential libasound2-dev libpulse-dev zlib1g-dev libedit-dev libvulkan-dev libudev-dev libevdev-dev
 
-#====universe:
 sudo apt-get install libopenal-dev libglew-dev libsdl2-2.0-0 libsdl2-dev
 
-#====universe (qt5 packages):
+# (qt5 packages):
 #====sudo apt-get install qtbase5-dev libqt5svg5-dev
+
 
 
 
@@ -120,17 +111,16 @@ printf '\n\n\n================\n'
 printf       '      PCSX2     \n'
 printf       '================\n\n'
 
-#====main:
-sudo apt-get install cmake gcc-multilib g++-multilib libaio-dev:i386 libbz2-dev:i386 libegl1-mesa-dev:i386 libgles2-mesa-dev:i386 libjpeg-dev:i386 zlib1g-dev:i386 libjack-jackd2-dev:i386 liblzma-dev:i386 libxml2-dev:i386 libpcap0.8-dev:i386
+sudo apt-get install cmake gcc-multilib g++-multilib libaio-dev:i386 libbz2-dev:i386 libegl1-mesa-dev:i386 libgles2-mesa-dev:i386 libjpeg-dev:i386 zlib1g-dev:i386 libjack-jackd2-dev:i386 liblzma-dev:i386 libxml2-dev:i386 libpcap0.8-dev:i386 libgtk2.0-dev:i386
 
-#====universe:
-sudo apt-get install libgtk2.0-dev:i386 libsdl1.2-dev:i386 libwxgtk3.0-gtk3-dev:i386 libsdl2-dev:i386 libportaudiocpp0:i386 portaudio19-dev:i386 libsoundtouch-dev:i386
+sudo apt-get install libsdl1.2-dev:i386 libwxgtk3.0-gtk3-dev:i386 libsdl2-dev:i386 libportaudiocpp0:i386 portaudio19-dev:i386 libsoundtouch-dev:i386
 
 # not in repos in 20.04+?
 #sudo apt-get install libglew-dev:i386
 
-#====multiverse (optional):
+# (optional):
 #====sudo apt-get install libcggl:i386 nvidia-cg-toolkit
+
 
 
 
@@ -138,20 +128,19 @@ printf '\n\n\n================\n'
 printf       '   DuckStation  \n'
 printf       '================\n\n'
 
-#====main:
 sudo apt-get install cmake libxrandr-dev pkg-config libevdev-dev libwayland-dev libwayland-egl-backend-dev
 
-#====main (optional):
+# (optional):
 sudo apt-get install libcurl4-openssl-dev libgbm-dev libdrm-dev
 
-#====universe:
 sudo apt-get install libsdl2-dev extra-cmake-modules
 
-#====universe (qt5 packages):
+# (qt5 packages):
 #====sudo apt-get install qtbase5-dev qtbase5-private-dev qtbase5-dev-tools qttools5-dev
 
-#====universe (optional):
+# (optional):
 #====sudo apt-get install ninja-build
+
 
 
 
