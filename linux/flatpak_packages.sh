b5780b85968eed06518a1f7205d28be2cb90129b
excursive <excursiveart@gmail.com>
2023-10-22 02:32:39 -0700

    edit

--- linux/flatpak_packages.sh
+++ linux/flatpak_packages.sh
@@ -23,6 +23,7 @@ flatpak install flathub org.inkscape.Inkscape
 
 #flatpak install flathub com.obsproject.Studio
 flatpak install flathub org.kde.kdenlive
+flatpak install flathub org.blender.Blender
 flatpak install flathub fr.natron.Natron
 
 #flatpak install flathub com.visualstudio.code
